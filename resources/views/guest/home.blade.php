955cbdc4ce9149b99d5ba49fe4199a68d012e2e1
AlessandroLembo <alessandrolembo2@gmail.com>
2023-03-07 23:30:03 +0100

    changes home blade

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('content')
-    <div class="jumbotron p-5 mb-4 bg-light rounded-3">
+    <h1 class="fs-1 text-center fw-bold my-5">HOME ADMIN PROJECT</h1>
+    {{-- <div class="jumbotron p-5 mb-4 bg-light rounded-3">
         <div class="container py-5">
             <div class="logo_laravel">
                 <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-25">
@@ -27,5 +28,5 @@
                 deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis
                 accusamus dolores!</p>
         </div>
-    </div>
+    </div> --}}
 @endsection
