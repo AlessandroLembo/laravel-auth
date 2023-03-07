81a55543ab6d667db9c3f5f2fc43202ac351ac1a
AlessandroLembo <alessandrolembo2@gmail.com>
2023-03-07 23:01:52 +0100

    print in show blade and create files scss

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,1 +1,27 @@
+@extends('layouts.app')
 
+@section('title', $project->name)
+
+@section('content')
+    <header class="text-center my-5 fs-1">{{ $project->name }}</header>
+    <div class="row">
+        <div class="col-6">
+            @if ($project->image)
+                <figure>
+                    <img src="{{ $project->image }}" alt="{{ $project->name }}">
+                </figure>
+            @endif
+
+        </div>
+        <div class="col-6">
+            <div class="d-flex flex-column align-items-start">
+                <h2>{{ $project->slug }}</h2>
+                <h4>{{ $project->description }}</h4>
+                <h4>{{ $project->project_for }}</h4>
+                <h4>{{ $project->web_platform }}</h4>
+                <h4>{{ $project->duration_project }}</h4>
+            </div>
+        </div>
+
+    </div>
+@endsection
