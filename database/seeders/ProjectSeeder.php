54a2e30ffdcb2f4a1c9c5e55e9b7a161edfa84c0
AlessandroLembo <alessandrolembo2@gmail.com>
2023-03-07 19:57:06 +0100

    branch project-seed to populate database

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,23 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $projects = config('projects');
+
+        foreach ($projects as $project) {
+            $new_project = new Project();
+
+            // alla proprietà dell'oggetto creato assegno il valore della chiave dell'array associativo
+
+            // $project->name = $project['name'];
+            // $project->description = $project['description'];
+            // $project->image = $project['image'];
+            // $project->slug = $project['slug'];
+            // $project->project_for = $project['project_for'];
+            // $project->web_platform = $project['web_platform'];
+            // $project->duration_project = $project['duration_project'];
+
+            $new_project->fill($project);
+            $new_project->save();
+        }
     }
 }
